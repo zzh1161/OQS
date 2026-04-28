fd8897eec9d6413d54e0aba9d2ff06cf60898e8a
zzh <1563833973@qq.com>
2026-04-28 12:00:35 +0800

    del bareMC and inchworm

--- NMSSE/bareMC/exp_val.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def compute_exp_val(psis, op):
-    
-    num = np.dot(np.conj(op@psis.T).flatten(), psis.T.flatten())
-    denom = np.dot(np.conj(psis).flatten(), psis.flatten())
-    return np.real_if_close(num/denom)
